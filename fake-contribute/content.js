f3386cd9f842b82127db8b60e06c3b05bddb832c
zo <77091655+ZozonTeq@users.noreply.github.com>
2022-02-03 10:45:34 +0900

    Delete fake-contribute directory

--- fake-contribute/content.js
+++
@@ -1,6 +0,0 @@
-for(let i = 0; i < document.getElementsByClassName("ContributionCalendar-day").length ;i++){
-    console.log(document.getElementsByClassName("ContributionCalendar-day")[i].getAttribute("data-level"))
-    document.getElementsByClassName("js-calendar-graph-svg")[0].getElementsByClassName("ContributionCalendar-day")[i].setAttribute("data-level",4)
-document.getElementsByClassName("js-calendar-graph-svg")[0].getElementsByClassName("ContributionCalendar-day")[i].setAttribute("data-count",4)
-
-}
